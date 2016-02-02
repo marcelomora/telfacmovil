37f7eb4b4cbaabea36d500e624ccf86c7e03f905
omarlemorac <java.diablo@gmail.com>
2016-02-02 12:33:39 -0500

    [ADD] Ping to server

--- app/src/main/java/com/accioma/telecosfacturamovil/activity/SettingsFormActivity.java
+++ app/src/main/java/com/accioma/telecosfacturamovil/activity/SettingsFormActivity.java
@@ -2,12 +2,19 @@ package com.accioma.telecosfacturamovil.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.accioma.telecosfacturamovil.R;
+import com.accioma.telecosfacturamovil.controller.ServiceHandler;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 
 /**
  * Created by marcelo on 1/27/16.
@@ -18,6 +25,7 @@ public class SettingsFormActivity extends AppCompatActivity{
     private EditText mEtUsername;
     private EditText mEtPassword;
     private Button mBtConfirm;
+    private TextView mJsonText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +35,36 @@ public class SettingsFormActivity extends AppCompatActivity{
         mEtUsername = (EditText) findViewById(R.id.editTextUsername);
         mEtPassword = (EditText) findViewById(R.id.editTextPassword);
         mBtConfirm = (Button) findViewById(R.id.btnSettingsConfirm);
+        mJsonText = (TextView) findViewById(R.id.jsonTextView);
 
         mBtConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(SettingsFormActivity.this, SettingsFormActivity.this.mEtHost.getText(),
+                String host = SettingsFormActivity.this.mEtHost.getText().toString();
+                Log.i("Settings Form", "excecuteCommand");
+                Runtime runtime = Runtime.getRuntime();
+                String message = "";
+                try{
+                    Process ipAddrProcess = runtime.exec("/system/bin/ping -c 1 " + host);
+                    int exitValue = ipAddrProcess.waitFor();
+                    Log.i("Settings Form", "ExitValue " + exitValue);
+                    if(exitValue==0){
+                        message = "Not connected";
+                    }else{
+                        message = "Connected";
+                        ServiceHandler sh = new ServiceHandler();
+                        String jsonStr = sh.makeServiceCall(host + ":5000", ServiceHandler.GET);
+                        //Log.d("Response", "> " + jsonStr);
+                        //SettingsFormActivity.this.mJsonText.setText(jsonStr);
+                    }
+                }catch(InterruptedException ignore){
+                    ignore.printStackTrace();
+                    Log.e("String Form", ignore.getMessage());
+                }catch(IOException e){
+                    e.printStackTrace();
+                    Log.i("Settings Form", e.getMessage());
+                }
+                Toast.makeText(SettingsFormActivity.this, message,
                         Toast.LENGTH_SHORT).show();
             }
         });
